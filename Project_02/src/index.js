9585d1f6251a310b005904f46f6b2df5a8a3a299
Zipeng Liu <66647718+zipeng-liu@users.noreply.github.com>
2023-12-05 23:33:42 -0800

    Delete Project_02 directory

--- Project_02/src/index.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const handler = require("./handler.js");
-
-const PORT = process.env.PORT || 3000;
-
-http
-  .createServer(handler)
-  .listen(PORT, () => console.log(`server is running at  ${PORT}`));
--- Project_02/src/util/util.js
+++
@@ -1,5 +0,0 @@
-const DEFAULT_HEADER = { "content-type": "text/html" };
-
-module.exports = {
-  DEFAULT_HEADER,
-};
